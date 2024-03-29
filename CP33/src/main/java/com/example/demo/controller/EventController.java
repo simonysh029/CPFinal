77f57ae06640bfff700dd3fc00d977ce688765f4
user <user@DESKTOP-F6QG0OJ>
2024-03-29 10:16:10 +0900

    [DBManager 코드충돌] - 염무원

--- CP33/src/main/java/com/example/demo/controller/EventController.java
+++ CP33/src/main/java/com/example/demo/controller/EventController.java
@@ -1,7 +1,16 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.demo.dao.EventDAO;
+import com.example.demo.vo.EventVO;
 
 import lombok.Setter;
 
@@ -9,10 +18,21 @@ import lombok.Setter;
 @Setter
 public class EventController {
 
+	@Autowired
+	public EventDAO dao;
+	@Autowired
+	public EventVO vo;
+	
+	//넘어가면서 list값 넣어 변환
 	@GetMapping("/page/listEvent")
-	public void listEvent() {		
+	public List<EventVO> listEvent() { 
+		return dao.findAll();
 	}
-	@GetMapping("/page/detailEvent")
-	public void detailEvent() {
+
+	//uri방식으로 받음
+	@GetMapping("/page/detailEvent/{e_no}")
+	public String detailEvent(@PathVariable("e_no") int e_no) {
+		return "/page/detailEvent";
 	}
+	
 }
---
+++ CP33/src/main/java/com/example/demo/dao/EventDAO.java
@@ -0,0 +1,16 @@
+package com.example.demo.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.db.DBManager;
+import com.example.demo.vo.EventVO;
+
+@Repository
+public class EventDAO {
+	public List<EventVO> findAll() {
+		return null;
+	}
+
+}
